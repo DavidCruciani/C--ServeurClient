a487521a4f1684e8935d018052cb320f1678b580
David Cruciani <da.cruciani@laposte.net>
2018-12-06 16:25:22 +0100

    Cercle

---
+++ Projet/Geometrie/Geometrie/Cercle.h
@@ -0,0 +1,39 @@
+#pragma once
+#include "Simple.h"
+
+using namespace std; 
+
+class Cercle :public Simple
+{
+private:
+	Vecteur2D vCentre;
+	double rayon;
+	Cercle(const Cercle&);
+
+	void operator=(const Cercle&) ;
+public:
+	Cercle();
+	Cercle(const double, const double, const double);
+	Cercle(const string, const double, const double, const double);
+	virtual ~Cercle();
+
+	void setCentre(const double, const double);
+	void setRayon(const double);
+	Vecteur2D getCentre() const;
+	double getRayon() const;
+
+	virtual Simple* clone()const { return new Cercle(*this); }
+	operator string()const;
+	bool operator ==(const Cercle&)const;
+	bool operator != (const Cercle & c)const { return !(*this == c); };
+
+	virtual double aire()const;
+
+	virtual void dessiner();
+	virtual void homothetie(const Vecteur2D&, const double);
+	virtual void rotation(const Vecteur2D&, const double);
+	virtual void translation(const Vecteur2D&);
+
+	friend ostream & operator <<(ostream & os, const Cercle & u);
+};
+
